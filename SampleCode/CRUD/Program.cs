3306b8b22612d568c4a0079c18bce9d1d3e68262
bjw <djqkdlem@gmail.com>
2016-07-16 16:04:04 +0900

    TEST  CRUD ADD

--- SampleCode/CRUD/Program.cs
+++ SampleCode/CRUD/Program.cs
@@ -10,6 +10,30 @@ namespace CRUD
     {
         static void Main(string[] args)
         {
+            // Insert
+            using (TestBiz biz = new TestBiz())
+            {
+                TestType type = new TestType();
+                type.Name = "가나다";
+                type.Number = 100;
+                biz.InsertTest(type);
+            }
+
+
+            // Select
+            List<TestType> testList = null;
+            using (TestBiz biz = new TestBiz())
+            {
+                testList = biz.SelectTestList();
+            }
+
+            foreach (var i in testList)
+            {
+                Console.WriteLine(i.Name + "  " + i.Number);
+            }
+
+
+
         }
     }
 }
